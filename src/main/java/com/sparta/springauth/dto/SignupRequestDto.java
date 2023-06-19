d405e9f99073e05a1a159383d5797a82667015dd
HyungCheolSim <sim34122@gmail.com>
2023-06-19 21:52:56 +0900

    pattern regex

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -1,13 +1,22 @@
 package com.sparta.springauth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    //@Email
+    @Pattern(regexp="^[a-zA-Z0-9!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank
+    //TODO @Pattern 을 사용해서 검증을 해봐라.
     private String email;
     private boolean admin = false;
     private String adminToken = "";
