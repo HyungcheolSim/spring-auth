d677f3299adb91b91d890b98ba85efadc3a8986b
HyungCheolSim <sim34122@gmail.com>
2023-06-19 14:45:45 +0900

    jwt 다루기

--- src/main/java/com/sparta/springauth/auth/AuthController.java
+++ src/main/java/com/sparta/springauth/auth/AuthController.java
@@ -1,5 +1,8 @@
 package com.sparta.springauth.auth;
 
+import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,14 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     @GetMapping("/create-cookie")
     public String createCookie(HttpServletResponse res) {
@@ -33,18 +42,14 @@ public class AuthController {
     }
 
     public static void addCookie(String cookieValue, HttpServletResponse res) {
-        try {
-            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
+        cookieValue = URLEncoder.encode(cookieValue, StandardCharsets.UTF_8).replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
 
-            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
-            cookie.setPath("/");
-            cookie.setMaxAge(30 * 60);
+        Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
+        cookie.setPath("/");
+        cookie.setMaxAge(30 * 60);
 
-            // Response 객체에 Cookie 추가
-            res.addCookie(cookie);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        // Response 객체에 Cookie 추가
+        res.addCookie(cookie);
     }
 
     @GetMapping("/create-session")
@@ -63,9 +68,42 @@ public class AuthController {
         // 세션이 존재할 경우 세션 반환, 없을 경우 null 반환
         HttpSession session = req.getSession(false);
 
-        String value = session.getAttribute(AUTHORIZATION_HEADER).toString(); // 가져온 세션에 저장된 Value 를 Name 을 사용하여 가져옵니다.
+        String value = String.valueOf(session.getAttribute(AUTHORIZATION_HEADER)); // 가져온 세션에 저장된 Value 를 Name 을 사용하여 가져옵니다.
         System.out.println("value = " + value);
 
         return "getSession : " + value;
+    }
+
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse res) {
+        // Jwt 생성
+        String token = jwtUtil.createToken("Robbie", UserRoleEnum.USER);
+
+        // Jwt 쿠키 저장
+        jwtUtil.addJwtToCookie(token, res);
+
+        return "createJwt : " + token;
+    }
+
+    @GetMapping("/get-jwt")
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if(!jwtUtil.validateToken(token)){
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        System.out.println("username = " + username);
+        // 사용자 권한
+        String authority =  String.valueOf(info.get(JwtUtil.AUTHORIZATION_KEY));
+        System.out.println("authority = " + authority);
+
+        return "getJwt : " + username + ", " + authority;
     }
 }
